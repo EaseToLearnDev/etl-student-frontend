31410ab7f08f99e0e901c3a4c3c324beb096b56c
akashigamedev <akashi.gamedev@gmail.com>
2025-07-09 22:58:07 +0530

    Storybook base setup

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,21 @@
-export default {
+import { StorybookConfig } from "@storybook/react-vite";
+
+const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    '@storybook/addon-docs',
-  ],
+  core: {
+    disableTelemetry: true,
+  },
+  addons: ["@storybook/addon-docs"],
   framework: {
     name: "@storybook/react-vite",
     options: {},
   },
   docs: {
-    defaultName: 'Documentation',
+    defaultName: "Documentation",
   },
   typescript: {
     reactDocgen: "react-docgen-typescript",
   },
 };
+
+export default config;
