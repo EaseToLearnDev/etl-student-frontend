51a9c9c7648e5cf2a9e1bb9e22244180a35e0da6
SuryaGautam <surya.gautam@gmail.com>
2025-08-21 14:58:46 +0530

    Resolve Test Stop Api Integration

--- src/features/test_simulator/services/handleContinueLater.ts
+++ src/features/test_simulator/services/handleContinueLater.ts
@@ -21,79 +21,76 @@ export const handleContinueLater = async () => {
     testMode = "Competitive Session";
   }
 
-  const obj = {
-    courseId: activeCourse?.courseId,
-    templateId: activeCourse?.templateId,
-    testName: testData?.testName,
-    testType: testData?.testType,
-    testId: testData?.testId,
-    testDetail: [
-      {
-        testId: testData?.testId,
-        testName: testData?.testName,
-        testType: testData?.testType,
-        testOption: testData?.testOption,
-        totalTime: testData?.totalTime,
-        ...(testData?.testType === 3 && {
-          noQuestionAttempt: testData?.noQuestionAttempt ?? 0,
-        }),
-        remainingTime: testData?.remainingTime,
-        lastQuestionIndex: testData?.lastQuestionIndex,
-        sectionLock: testData?.sectionLock,
-        bloom: testData?.bloom,
-        sectionSet: testData?.sectionSet.map((item) => ({
-          questionNumbers: item.questionNumbers.map((i) => ({
-            questionIndex: i.questionIndex,
-            questionId: i.questionId,
-          })),
-          ...(testData?.testType === 3 && {
-            sectionName: item.sectionName ?? "",
-          }),
+  const testDetail = [
+    {
+      testId: testData?.testId,
+      testName: testData?.testName,
+      testType: testData?.testType,
+      testOption: testData?.testOption,
+      totalTime: testData?.totalTime,
+      ...(testData?.testType === 3 && {
+        noQuestionAttempt: testData?.noQuestionAttempt ?? 0,
+      }),
+      remainingTime: testData?.remainingTime,
+      lastQuestionIndex: testData?.lastQuestionIndex,
+      sectionLock: testData?.sectionLock,
+      bloom: testData?.bloom,
+      sectionSet: testData?.sectionSet.map((item) => ({
+        questionNumbers: item.questionNumbers.map((i) => ({
+          questionIndex: i.questionIndex,
+          questionId: i.questionId,
         })),
-        questionSet:
-          testData?.questionSet.map((item) => {
-            const baseObj: any = {
-              questionId: item.questionId,
-              questionDisplayId: item.questionDisplayId,
-              questionType: item.questionType,
-              questionTypeLabel: item.questionTypeLabel,
-              topicId: item.topicId,
-              timeSpent: item.timeSpent,
-              correctAnswerMarks: item.correctAnswerMarks,
-              incorrectAnswerMarks: item.incorrectAnswerMarks,
-              notAnswerMarks: item.notAnswerMarks,
-              questionBody: item.questionBody,
-              responseChoice: item.responseChoice.map((i) => ({
-                responseId: i.responseId,
-                responseText: i.responseText,
-              })),
-              backgroundImg: item.backgroundImg,
-              cssName: item.cssName,
+        ...(testData?.testType === 3 && {
+          sectionName: item.sectionName ?? "",
+        }),
+      })),
+      questionSet:
+        testData?.questionSet.map((item) => {
+          const baseObj: any = {
+            questionId: item.questionId,
+            questionDisplayId: item.questionDisplayId,
+            questionType: item.questionType,
+            questionTypeLabel: item.questionTypeLabel,
+            topicId: item.topicId,
+            timeSpent: item.timeSpent,
+            correctAnswerMarks: item.correctAnswerMarks,
+            incorrectAnswerMarks: item.incorrectAnswerMarks,
+            notAnswerMarks: item.notAnswerMarks,
+            questionBody: item.questionBody,
+            responseChoice: item.responseChoice.map((i) => ({
+              responseId: i.responseId,
+              responseText: i.responseText,
+            })),
+            backgroundImg: item.backgroundImg,
+            cssName: item.cssName,
+            studentResponse:
+              questionResponseMap[item.questionId]?.responseId || "",
+          };
 
-              studentResponse:
-                questionResponseMap[item.questionId]?.responseId || "",
-            };
+          if (testData?.testType === 3) {
+            baseObj.sectionId = item.sectionId;
+            baseObj.sectionName = item.sectionName;
+            baseObj.sectionOrder = item.sectionOrder;
+          } else {
+            baseObj.bloomId = 0;
+          }
 
-            if (testData?.testType === 3) {
-              baseObj.sectionId = item.sectionId;
-              baseObj.sectionName = item.sectionName;
-              baseObj.sectionOrder = item.sectionOrder;
-            } else {
-              baseObj.bloomId = 0;
-            }
-
-            return baseObj;
-          }) ?? [],
-      },
-    ],
-    helpCounter: 0,
-    testMode: testMode,
-  };
+          return baseObj;
+        }) ?? [],
+    },
+  ];
 
   const { loginId, token } = studentData;
 
   const data = new FormData();
-  data.append("teststop", JSON.stringify(obj));
+  data.append("courseId", String(activeCourse?.courseId));
+  data.append("templateId", String(activeCourse?.templateId));
+  data.append("testName", testData?.testName ?? "");
+  data.append("testType", String(testData?.testType));
+  data.append("testId", String(testData?.testId));
+  data.append("testDetail", JSON.stringify(testDetail));
+  data.append("helpCounter", "0");
+  data.append("testMode", testMode);
 
   const res = await testStop({ data, loginId, token });
   if (!res) {
