a545fdcf933c2a7f93ee1a637d171029a98e4c70
SuryaGautam <surya.gautam@gmail.com>
2025-09-02 16:17:00 +0530

    Modify the Toast

--- src/components/Toast.tsx
+++ src/components/Toast.tsx
@@ -1,11 +1,14 @@
-import React, { type JSX } from "react";
+import React, { useEffect, useState, type JSX } from "react";
 import {
   MdCheckCircle,
-  MdError,
   MdInfo,
   MdWarning,
   MdClose,
+  MdBuild,
 } from "react-icons/md";
+import cn from "../utils/classNames";
+import Button from "./Button";
+import { BiError } from "react-icons/bi";
 
 export enum ToastType {
   SUCCESS = "success",
@@ -16,53 +19,105 @@ export enum ToastType {
 }
 
 interface ToastProps {
-  type: ToastType;
+  type?: ToastType;
   title: string;
   description?: string;
   button?: string;
-  onClose?: () => void;
+  duration?: number;
+  className?: string;
 }
 
 const colorMap: Record<ToastType, string> = {
-  [ToastType.SUCCESS]: "bg-[var(--sb-green-haze-bg-disabled)] text-[var(--sb-green-haze-bg-active)] border-[var(--sb-green-haze-bg-active)]",
-  [ToastType.DANGER]: "bg-[var(--sb-valencia-bg-disabled)] text-[var(--sb-valencia-bg-active)] border-[var(--sb-valencia-bg-active)]",
-  [ToastType.PRIMARY]: "bg-[var(--sb-ocean-bg-disabled)] text-[var(--sb-ocean-bg-active)] border-[var(--sb-ocean-bg-active)]",
-  [ToastType.WARNING]: "bg-[var(--sb-sunglow-bg-disabled)] text-[var(--sb-sunglow-bg-active)] border-[var(--sb-sunglow-bg-active)]",
-  [ToastType.PATCH]: "bg-[var(--sb-neutral-bg-disabled)] text-[var(--sb-neutral-bg-active)] border-[var(--sb-neutral-bg-active)]",
+  [ToastType.SUCCESS]:
+    "bg-[var(--sb-green-haze-bg-disabled)] text-[var(--sb-green-haze-bg-active)] border-[var(--sb-green-haze-bg-active)]",
+  [ToastType.DANGER]:
+    "bg-[var(--sb-valencia-bg-disabled)] text-[var(--sb-valencia-bg-active)] border-[var(--sb-valencia-bg-active)]",
+  [ToastType.PRIMARY]:
+    "bg-[var(--sb-ocean-bg-disabled)] text-[var(--sb-ocean-bg-active)] border-[var(--sb-ocean-bg-active)]",
+  [ToastType.WARNING]:
+    "bg-[var(--sb-sunglow-bg-disabled)] text-[var(--sb-sunglow-bg-active)] border-[var(--sb-sunglow-bg-active)]",
+  [ToastType.PATCH]:
+    "bg-[var(--sb-pumpkin-bg-disabled)] text-[var(--sb-pumpkin-bg-active)] border-[var(--sb-pumpkin-bg-active)]",
 };
 
 const iconMap: Record<ToastType, JSX.Element> = {
-  [ToastType.SUCCESS]: <MdCheckCircle className="text-[var(--sb-green-haze-bg-active)]" />,
-  [ToastType.DANGER]: <MdError className="text-[var(--sb-valencia-bg-active)]" />,
-  [ToastType.PRIMARY]: <MdInfo className="text-[var(--sb-ocean-bg-active)] text-xl" />,
-  [ToastType.WARNING]: <MdWarning className="text-[var(--sb-sunglow-bg-active)] text-xl" />,
-  [ToastType.PATCH]: <MdInfo className="text-[var(--sb-neutral-bg-active)] text-xl" />,
+  [ToastType.SUCCESS]: (
+    <MdCheckCircle className="text-[var(--sb-green-haze-bg-active)] text-xl" />
+  ),
+  [ToastType.DANGER]: (
+    <BiError className="text-[var(--sb-valencia-bg-active)] text-xl" />
+  ),
+  [ToastType.PRIMARY]: (
+    <MdInfo className="text-[var(--sb-ocean-bg-active)] text-xl" />
+  ),
+  [ToastType.WARNING]: (
+    <MdWarning className="text-[var(--sb-sunglow-bg-active)] text-xl" />
+  ),
+  [ToastType.PATCH]: (
+    <MdBuild className="text-[var(--sb-pumpkin-bg-active)] text-xl" />
+  ),
 };
 
 export const Toast: React.FC<ToastProps> = ({
-  type,
+  type = ToastType.PRIMARY,
   title,
   description,
   button,
-  onClose,
+  duration = 3000,
+  className,
 }) => {
+  const [visible, setVisible] = useState(true);
+
+  const handleOnClick = () => {
+    setVisible(false);
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, duration);
+
+    return () => clearTimeout(timer);
+  }, [duration]);
+
+  if (!visible) return null;
+
   return (
     <div
-      className={`flex items-start gap-3 border rounded-xl shadow-lg p-4 w-80 ${colorMap[type]}`}
+      className={cn(
+        "z-9999 fixed top-5 right-5 w-md items-start flex gap-3 border rounded-xl shadow-lg p-4",
+        colorMap[type],
+        className
+      )}
     >
-      {iconMap[type]}
       <div className="flex-1">
-        <h4 className="font-semibold">{title}</h4>
-        {description && <p className="text-sm opacity-80">{description}</p>}
-        {button && (
-          <button className="mt-2 px-3 py-1 rounded bg-gray-800 text-white text-sm">
-            {button}
-          </button>
-        )}
+        <div className="flex items-start gap-2">
+          <p>{iconMap[type]}</p>
+
+          <div className="flex flex-col gap-1">
+            <h6>{title}</h6>
+            {description && <p>{description}</p>}
+            {button && (
+              <Button
+                style="secondary"
+                className={`hover:bg-opacity-80 w-fit mt-2 px-3 py-1 rounded ${colorMap[type]}`}
+              >
+                <p className="font-medium">{button}</p>
+              </Button>
+            )}
+          </div>
+          
+        </div>
       </div>
-      <button onClick={onClose}>
-        <MdClose />
-      </button>
+
+      <div
+        onClick={handleOnClick}
+        className={cn(
+          "fixed top-7 right-7 w-[40px] h-[40px] aspect-square flex justify-center items-center cursor-pointer"
+        )}
+      >
+        <MdClose size={20} />
+      </div>
     </div>
   );
 };
