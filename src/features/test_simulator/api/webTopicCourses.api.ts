d193757759b36184d80d589e2bd40ec65b829d9a
SuryaGautam <surya.gautam@gmail.com>
2025-09-03 17:07:53 +0530

    Add Guest Course Cards

--- src/features/test_simulator/api/webTopicCourses.api.ts
+++ src/features/test_simulator/api/webTopicCourses.api.ts
@@ -2,14 +2,15 @@ import { makeRequest } from "../../../utils/http";
 
 export const webTopicCourses = async (topicId: number) => {
   const res = await makeRequest("get", "/app/web-topic-courses", null, {
-    parmas: {
+    params: {
       topicId: topicId,
       websiteId: 1,
     },
+
     headers: {
       "Content-Type": "multipart/mixed",
     },
   });
 
-  return res?.data ?? null;
+  return res?.data?.obj ?? null;
 };
