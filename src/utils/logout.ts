3b9cffa2e82a3d129fae43e2ef2c7bad810544a4
akashigamedev <akashi.gamedev@gmail.com>
2025-09-09 22:48:40 +0530

    base url fix

--- src/utils/logout.ts
+++ src/utils/logout.ts
@@ -7,7 +7,7 @@ import { useStudentStore } from "../features/shared/hooks/useStudentStore";
 const logout = () => {
   const { reset } = useStudentStore.getState();
   reset();
-  window.location.href = "/";
+  window.location.href = "/login";
 };
 
 export default logout;
