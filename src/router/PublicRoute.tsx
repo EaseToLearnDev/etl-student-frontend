2a8d1684bf3326667f8cd6172a274b8910c03494
akashigamedev <akashi.gamedev@gmail.com>
2025-10-28 13:36:03 +0530

    Fixed public route navigation

--- src/router/PublicRoute.tsx
+++ src/router/PublicRoute.tsx
@@ -1,4 +1,3 @@
-
 // React
 import { Navigate } from "react-router-dom";
 
@@ -9,9 +8,17 @@ import { useStudentStore } from "../features/shared/hooks/useStudentStore";
  * Redirects authenticated students to the home page, otherwise renders children.
  */
 const PublicRoute = ({ children }: { children: React.ReactNode }) => {
-  const studentData = useStudentStore(state => state.studentData);
+  const studentData = useStudentStore((state) => state.studentData);
 
-  return studentData?.token ? <Navigate to="/" replace /> : children;
+  if (!studentData?.token) {
+    return children;
+  } else {
+    if (studentData?.courses && studentData?.courses?.length > 0) {
+      return <Navigate to="/" replace />;
+    } else {
+      return <Navigate to="/selectyourcourse" replace />;
+    }
+  }
 };
 
 export default PublicRoute;
